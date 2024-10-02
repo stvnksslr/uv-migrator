3a47096e564114a0dde0f190f578b340d1e10399
Steven Kessler <stvnksslr@gmail.com>
2024-10-02 09:48:24 -0400

    fix(go releaser hacks):

--- .goreleaser_hook.sh
+++ .goreleaser_hook.sh
@@ -13,9 +13,12 @@ esac
 case $go_os in
     linux) rust_os='linux' ;;
     darwin) rust_os='apple-darwin' ;;
-    windows) rust_os='windows' ;;
+    windows) rust_os='pc-windows-msvc' ;;
     *) echo "unknown os: $go_os" && exit 1 ;;
 esac
 
-# Find artifacts and uncompress in the corresponding directory
-find artifacts -type f -name "*${rust_arch}*${rust_os}*" -exec unzip -d dist/${project_name}_${go_os}_${go_arch} {} \;
+# Construct the expected artifact name
+artifact_name="${rust_arch}-${rust_os}-${project_name}"
+
+# Find the artifact and uncompress in the corresponding directory
+find artifacts -type f -name "$artifact_name" -exec unzip -d dist/${project_name}_${go_os}_${go_arch} {} \;
