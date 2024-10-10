fb8af2100162cd49838d1183b5a8a954ccb2ce39
Steven Kessler <stvnksslr@gmail.com>
2024-10-10 16:29:23 -0400

    feat(pip): requirements.txt is handled better + cases with no existing pyproject.toml is handled

--- src/types.rs
+++ src/types.rs
@@ -3,14 +3,7 @@ use std::collections::HashMap;
 
 #[derive(Deserialize, Debug)]
 pub struct PyProject {
-    pub project: Option<Project>,
     pub tool: Option<Tool>,
-}
-
-#[derive(Deserialize, Debug)]
-pub struct Project {
-    pub dependencies: Option<toml::Value>,
-    pub optional_dependencies: Option<HashMap<String, toml::Value>>,
 }
 
 #[derive(Deserialize, Debug)]
